f9a083a538b04a27db1ab5f4fe9edcf202103de1
Dermot O' Halloran <dermtt@gmail.com>
2015-11-18 13:47:29 +0000

    Update container-server.py
    updated functions

--- container-server.py
+++ container-server.py
@@ -11,25 +11,19 @@ app = Flask(__name__)
 def index():
     return """
 Available API endpoints:
-
 GET /containers                     List all containers
 GET /containers?state=running      List running containers (only)
 GET /containers/<id>                Inspect a specific container
 GET /containers/<id>/logs           Dump specific container logs
 GET /images                         List all images
-
-
 POST /images                        Create a new image
 POST /containers                    Create a new container
-
 PATCH /containers/<id>              Change a container's state
 PATCH /images/<id>                  Change a specific image's attributes
-
 DELETE /containers/<id>             Delete a specific container
 DELETE /containers                  Delete all containers (including running)
 DELETE /images/<id>                 Delete a specific image
 DELETE /images                      Delete all images
-
 """
 
 @app.route('/containers', methods=['GET'])
@@ -37,9 +31,8 @@ def containers_index():
     """
     List all containers
  
-    curl -s -X GET -H 'Accept: application/json' http://localhost:8080/containers | python -mjson.tool
-    curl -s -X GET -H 'Accept: application/json' http://localhost:8080/containers?state=running | python -mjson.tool
-
+    curl -s -X GET -H 'Accept: application/json' ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers | python -mjson.toolv
+    curl -s -X GET -H 'Accept: application/json' ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers?state=running | python -mjson.tool
     """
     if request.args.get('state') == 'running':
         output = docker('ps')
@@ -48,59 +41,56 @@ def containers_index():
     resp = json.dumps(docker_ps_to_array(output))
     return Response(response=resp, mimetype="application/json")
 
-
 @app.route('/images', methods=['GET'])
 def images_index():
     """
-    List all images 
-    
-    Complete the code below generating a valid response. 
-    """
-    curl -s -X GET -H 'Accept: application/json' http://localhost:8080/images | python -mjson.tool
-    curl -s -X GET -H 'Accept: application/json' http://localhost:8080/images?state=running | python -mjson.tool
-
+    List all images
+ 
+    curl -s -X GET -H 'Accept: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers | python -mjson.tool
+    curl -s -X GET -H 'Accept: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers?state=running | python -mjson.tool
     """
     if request.args.get('state') == 'running':
-        output = docker('ps')
+        output = docker('images')
     else:
-        output = docker('ps', '-a')
-    resp = json.dumps(docker_ps_to_array(output))
-    
-    
+        output = docker('images', '-a')
+    resp = json.dumps(docker_images_to_array(output))
     return Response(response=resp, mimetype="application/json")
 
 @app.route('/containers/<id>', methods=['GET'])
 def containers_show(id):
     """
     Inspect specific container
-
-    """
-    curl -s -X GET -H 'Accept: application/json' http://localhost:8080/containers/id
-    | python -mjson.tool
     
-
+    curl -s -X GET$ -H 'Accept: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers | python -mjson.tool 
+    
     """
     if request.args.get('state') == 'running':
-        output = docker('ps')
+        output = docker('inspect')
     else:
-        output = docker('ps', '-a')
-    resp = json.dumps(docker_ps_to_array(output))
+        output = docker('inspect', id)
+    resp = json.dumps(docker_logs_to_object(id,output))
+
     return Response(response=resp, mimetype="application/json")
 
 @app.route('/containers/<id>/logs', methods=['GET'])
 def containers_log(id):
     """
     Dump specific container logs
-
     """
-    resp = ''
-    return Response(response=resp, mimetype="application/json")
 
+    if request.args.get('state') == 'running':
+        output = docker('logs',id)
+    else:
+        output = docker('logs',id)
+    resp = json.dumps(docker_logs_to_object(id,output))
+
+    return Response(response=resp, mimetype="application/json")
 
 @app.route('/images/<id>', methods=['DELETE'])
 def images_remove(id):
     """
     Delete a specific image
+    curl -s -X DELETE -H 'Accept: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/images/<imgID> | python -mjson.tool
     """
     docker ('rmi', id)
     resp = '{"id": "%s"}' % id
@@ -110,78 +100,88 @@ def images_remove(id):
 def containers_remove(id):
     """
     Delete a specific container - must be already stopped/killed
-
+    curl -s -X DELETE -H 'Accept: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers/<contID> | python -mjson.tool
     """
-    resp = ''
+    docker ('stop', id)
+    docker ('rm', id)
+    resp = '{"id": "%s"}' % id
     return Response(response=resp, mimetype="application/json")
 
-@app.route('/containers', methods=['DELETE'])
+@app.route('/containersDel', methods=['DELETE'])
 def containers_remove_all():
     """
     Force remove all containers - dangrous!
-
+    curl -s -X DELETE -H 'Accept: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containersDel | python -mjson.tool
     """
-    resp = ''
+    all = docker_ps_to_array(docker('ps', '-a'))
+    for i in all:
+        docker ('stop',i['id'])
+        docker('rm', i['id'])
+    resp = '{"count": "%d"}' %len(all)
     return Response(response=resp, mimetype="application/json")
 
-@app.route('/images', methods=['DELETE'])
+@app.route('/imagesDel', methods=['DELETE'])
 def images_remove_all():
     """
     Force remove all images - dangrous!
-
+    curl -s -X DELETE -H 'Accept: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/imagesDel | python -mjson.tool
     """
- 
-    resp = ''
+    all = docker_images_to_array(docker('images'))
+    for i in all:
+        docker('rmi', i['id'])
+    resp = '{"count": "%d"}' %len(all)
     return Response(response=resp, mimetype="application/json")
-
 
 @app.route('/containers', methods=['POST'])
 def containers_create():
     """
     Create container (from existing image using id or name)
-
-    curl -X POST -H 'Content-Type: application/json' http://localhost:8080/containers -d '{"image": "my-app"}'
-    curl -X POST -H 'Content-Type: application/json' http://localhost:8080/containers -d '{"image": "b14752a6590e"}'
-    curl -X POST -H 'Content-Type: application/json' http://localhost:8080/containers -d '{"image": "b14752a6590e","publish":"8081:22"}'
-
+    curl -X POST -H 'Content-Type: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers -d '{"image": "my-app"}' | python -mjson.tool
+    curl -X POST -H 'Content-Type: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers -d '{"image": "b14752a6590e"}' | python -mjson.tool
+    curl -X POST -H 'Content-Type: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers -d '{"image": "b14752a6590e","publish":"8081:22"}' | python -mjson.tool
     """
     body = request.get_json(force=True)
     image = body['image']
     args = ('run', '-d')
+    try:
+        publish = body['publish']
+        args = args + ('-p', publish)
+    except:
+        pass
     id = docker(*(args + (image,)))[0:12]
     return Response(response='{"id": "%s"}' % id, mimetype="application/json")
+
+
 
 
 @app.route('/images', methods=['POST'])
 def images_create():
     """
     Create image (from uploaded Dockerfile)
-
-    curl -H 'Accept: application/json' -F file=@Dockerfile http://localhost:8080/images
-
+    curl -H 'Accept: application/json' -F file=@Dockerfile http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/images | python -mjson.tool
     """
+    body = request.get_json(force=True)
+    file = body['file']
     dockerfile = request.files['file']
-    
+
+    args = ('build','--rm=true','-f','Dockerfile','.')
+    docker(*(args))
     resp = ''
     return Response(response=resp, mimetype="application/json")
-
-
-
-
 @app.route('/containers/<id>', methods=['PATCH'])
 def containers_update(id):
     """
     Update container attributes (support: state=running|stopped)
-
-    curl -X PATCH -H 'Content-Type: application/json' http://localhost:8080/containers/b6cd8ea512c8 -d '{"state": "running"}'
-    curl -X PATCH -H 'Content-Type: application/json' http://localhost:8080/containers/b6cd8ea512c8 -d '{"state": "stopped"}'
-
+    curl -X PATCH -H 'Content-Type: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers/b6cd8ea512c8 -d '{"state": "running"}'
+    curl -X PATCH -H 'Content-Type: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/containers/b6cd8ea512c8 -d '{"state": "stopped"}'
     """
     body = request.get_json(force=True)
     try:
         state = body['state']
         if state == 'running':
             docker('restart', id)
+        else:
+            docker('stop', id)
     except:
         pass
 
@@ -192,11 +192,14 @@ def containers_update(id):
 def images_update(id):
     """
     Update image attributes (support: name[:tag])  tag name should be lowercase only
-
-    curl -s -X PATCH -H 'Content-Type: application/json' http://localhost:8080/images/7f2619ed1768 -d '{"tag": "test:1.0"}'
-
+    curl -s -X PATCH -H 'Content-Type: application/json' http://ec2-54-173-217-212.compute-1.amazonaws.com:8080/images/d7e52e156daf -d '{"tag": "test:1.0"}'
     """
-    resp = ''
+
+    body = request.get_json(force=True)
+    name = body['tag']
+    docker('tag', id, name)
+
+    resp = '{"id": "%s"}' % id
     return Response(response=resp, mimetype="application/json")
 
 
